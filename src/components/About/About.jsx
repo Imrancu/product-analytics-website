38f804ae1c656013415fd96502aa0e058630f46e
imrancu <mdimran3772@gmail.com>
2022-04-05 01:15:16 +0600

    Blogs part done

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import Me from '../../images/me.png';
+import './About.css';
 const About = () => {
     return (
-        <div>
-            <h2>This is About</h2>
+        <div className='about-container'>
+            <h2 className='text-4xl font-bold my-5 text-gray-600'>About Me</h2>
+            <div className='about'>
+                <img src={Me} alt="" />
+                <p>This is Mohammad Imran. Programming is my dream job that I enjoy so much. I also love to design social media and graphic elements via Adobe Creative softwares. I learnt a lots of new things with Programing-Hero.</p>
+            </div>
         </div>
     );
 };
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import './Blogs.css';
 
 const Blogs = () => {
     return (
-        <div>
-            <h2>This is blogs</h2>
+        <div className="blogs">
+            <div className='card'>
+                <h3 className='text-4xl font-semibold text-gray-600'>What is context API</h3>
+                <p className='mt-4'>Context was release in version 16.3 of Reactjs. Context API is a useful, new innovation in react that we can use as an alternative of props drilling. Props drilling is a boring and perfomance consuming term. On the other hand, Context API is super powerful thing in reactjs. By using context, we can very easily pass data to every where. Finnaly, If we use context API we get the best performance rather using props.</p>
+            </div>
+            <div className='card'>
+                <h3 className='text-4xl font-semibold text-gray-600'>What are Semantic tags</h3>
+                <p className='mt-4'>Semantic Tags mean HTML Semantic Elements that used to define the different parts of an application. Semantic tags have many benefits. 
+                <ul>
+                    <li>Some are below:</li>
+                    <li>It helps to rank your website on search engine.</li>
+                    <li>Increase user accessibility</li>
+                    <li>Helps to coders to understand the meaning of the code.</li>
+                </ul>
+                </p>
+            </div>
         </div>
     );
 };
