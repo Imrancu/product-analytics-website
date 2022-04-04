098d723950f6cb4567cb454c81545cb73d9fb70b
imrancu <mdimran3772@gmail.com>
2022-04-04 23:40:10 +0600

    All functionalities are done

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,26 +1,21 @@
 import React from 'react';
-<<<<<<< HEAD
+import { useNavigate } from 'react-router-dom';
 import useReview from '../../hooks/useReview';
 import Review from '../Review/Review';
-=======
->>>>>>> c4d21ec7cffc18c945cb9e511761fe60a0b6c012
 import './Reviews.css';
 
 const Reviews = () => {
     const [reviews] = useReview([])
+    const navigate = useNavigate()
     return (
-<<<<<<< HEAD
         <div className='all-center'>
             <h2 className='hover text-5xl font-bold text-gray-600 my-8'>What They Say!!</h2>
             <div className='card-container'>
-            {
-                reviews.map(review => <Review key={review.id} review={review}></Review>)
-            }
-        </div>
-=======
-        <div>
-            <h2 className='hover text-5xl font-bold text-gray-600'>Customar Reviews</h2>
->>>>>>> c4d21ec7cffc18c945cb9e511761fe60a0b6c012
+                {
+                    reviews.map(review => <Review key={review.id} review={review}/>)
+                }
+            </div>
+            <button onClick={() => navigate(-1)} className='btn'>Back To Home</button>
         </div>
     );
 };
